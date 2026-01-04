5fb44a1ef0836044887b426a29ad7b045ff630cb
Sedat999s <123291899+Sedat999s@users.noreply.github.com>
2026-01-04 09:29:37 +0300

    commit

--- tapsiriq.py
+++ tapsiriq.py
@@ -1,5 +1,5 @@
 # 1
-s="aaabbccccd"; r=""; c=1
+s="abcd"; r=""; c=1
 for i in range(1,len(s)):
     if s[i]==s[i-1]: c+=1
     else: r+=s[i-1]+str(c); c=1
@@ -7,7 +7,7 @@ r+=s[-1]+str(c)
 print(r)
 
 # 2
-s="hellooo!!!"; r=s[0]
+s="hello"; r=s[0]
 for i in range(1,len(s)):
     if s[i]!=s[i-1]: r+=s[i]
 print(r)
